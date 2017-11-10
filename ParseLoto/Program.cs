b6e8429e927ff44ab63bc6ac50946d1721ea7529
IvanStrelkov <vnstrlkv@gmail.com>
2017-11-10 17:31:41 +0700

    add

--- ParseLoto/Program.cs
+++ ParseLoto/Program.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.PhantomJS;
 
-namespace ParseLoto
+namespace WebsiteContentParser
 {
+  
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+
         {
+            string st = GetNumberOrders("https://www.stoloto.ru/ruslotto/game?lastdraw");
+            Console.WriteLine(st);
+        }
+
+       static public string GetNumberOrders(string URL)
+        {
+            /*
+            WebB webControl1;
+            webControl1.Source = new Uri(URL);
+            while (webControl1.IsLoading)
+            {
+                Application.DoEvents();
+            }
+            return webControl1.ExecuteJavascriptWithResult("document.documentElement.outerHTML").ToString();
+
+             */
+            PhantomJSDriverService service = PhantomJSDriverService.CreateDefaultService();
+            service.IgnoreSslErrors = true;
+            service.LoadImages = false;
+            service.ProxyType = "none";
+
+            IWebDriver driver = new PhantomJSDriver(service);
+            driver.Navigate().GoToUrl(URL);
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+    //        Console.WriteLine(driver.PageSource);
+            doc.LoadHtml(driver.PageSource);
+            string result = "";
+          
+            doc.OptionFixNestedTags = true;
+           HtmlNode node = doc.DocumentNode.SelectSingleNode("//*[@class='bingo_ticket ruslotto']");
+            result += node.InnerHtml;
+            return result;
         }
     }
+
+
+
 }
